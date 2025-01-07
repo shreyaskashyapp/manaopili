0c0069c66fa851d9556a136430a56eeb46425bbc
ragritti <rittirag@gmail.com>
2025-01-07 22:57:00 +0530

    added quotes

--- src/app/components/pdf-components/pdf-footer.js
+++ src/app/components/pdf-components/pdf-footer.js
@@ -4,7 +4,7 @@ export function PDFFooter() {
   return (
     <div className="bg-black text-white p-4 w-full">
       <div className="text-center">
-        <p className="mb-2">© 2025 Mana'o Pili LLC. All rights reserved.</p>
+        <p className="mb-2">{"© 2025 Mana'o Pili LLC. All rights reserved."}</p>
         <p className="text-gray-400 text-sm">
           This assessment report is confidential and intended solely for the use of the individual or entity to whom it is addressed.
         </p>
