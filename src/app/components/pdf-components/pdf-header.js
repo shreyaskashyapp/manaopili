9e6c109b563fbd9f7826ca2a58e7ad890f2c003c
ragritti <116258047+ragritti@users.noreply.github.com>
2025-04-17 20:12:40 +0530

    fixed pdf header

--- src/app/components/pdf-components/pdf-header.js
+++ src/app/components/pdf-components/pdf-header.js
@@ -2,12 +2,12 @@
 
 export function PDFHeader() {
   return (
-    <div 
-      className="w-full h-[150px] text-white px-10 py-6 bg-gradient-to-br from-neutral-900 via-[#455CFF] to-zinc-700">
-      <div className="space-y-2">
+    <div
+      className="w-full h-full text-white px-10 py-10 bg-gradient-to-br from-neutral-900 via-[#455CFF] to-zinc-700">
+      <div className="space-y-1">
         <h1 className="text-xl font-light tracking-wide">{`Mana'o Pili LLC`}</h1>
         <h2 className="text-4xl leading-tight font-semibold max-w-3xl">
-        ServiceNow Technology Workflows (Tx) - Results
+          ServiceNow Technology Workflows (Tx) - Results
         </h2>
       </div>
     </div>
