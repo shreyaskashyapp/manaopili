937c13f8f805805ee0a0325edf727180dffa0bd4
ragritti <116258047+ragritti@users.noreply.github.com>
2025-05-05 16:41:53 +0530

    survey instruction changes

--- src/app/components/survey-instruction.js
+++ src/app/components/survey-instruction.js
@@ -3,22 +3,22 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 
 export default function SurveyInstructions({ data }) {
     return (
-        <div className="container flex flex-col gap-3 mx-auto p-4 md:py-1 bg-[#141414]">
+        <div className="container flex flex-col gap-2 mx-auto p-2 md:py-1 bg-[#141414]">
             <p className="text-gray-400 max-w-2xl mx-auto text-center">
                 {data?.info?.description}
             </p>
-            <div className="pt-3 md:flex-row gap-4 flex flex-wrap justify-center items-center ">
+            <div className="pt-2 md:flex-row gap-4 flex flex-wrap justify-center items-center ">
                 {
                     data?.scores?.map((item, index) => (
-                        <Card key={index} className="md:w-[200px] md:h-[200px] w-[160px] h-[200px] p-1 border-none bg-gradient-to-b text-center from-[#141414] to-zinc-900 shadow-sm rounded-xl ">
-                            <CardHeader className="p-2">
-                                <CardTitle className="text-[#455CFF] text-4xl font-normal">{item.level}</CardTitle>
+                        <Card key={index} className="w-[150px] md:w-[200px] h-[140px] p-1 border-[#141414] bg-gradient-to-b text-center from-[#141414] to-zinc-900  rounded-xl ">
+                            <CardHeader className="px-2 py-0.5">
+                                <CardTitle className="text-[#455CFF] text-2xl font-normal">{item.level}</CardTitle>
                             </CardHeader>
-                            <CardContent className="flex flex-col gap-2 px-2">
-                                <CardDescription className="text-gray-300 text-lg" >
+                            <CardContent className="flex flex-col gap-0.5 px-0.5">
+                                <CardDescription className="text-gray-300 text-base" >
                                     {item.title}
                                 </CardDescription>
-                                <CardDescription className="text-gray-400 text-sm" >
+                                <CardDescription className="text-gray-400 text-xs" >
                                     {item.description}
                                 </CardDescription>
                             </CardContent>
@@ -26,18 +26,18 @@ export default function SurveyInstructions({ data }) {
                     ))
                 }
             </div>
-            <div className="flex flex-col text-left gap-3 lg:px-28 md:px-10 px-2 pt-3">
-                <p className="text-gray-300 text-lg">
+            <div className="flex flex-col justify-center items-center text-left gap-3 lg:px-28 md:px-10 px-2 pt-2">
+                <p className="text-gray-300 text-base">
                     Provide seperate scores for:
                 </p>
-                <ul className="flex flex-col gap-3 ">
+                <ul className="flex flex-col md:flex-row w-full gap-3 md:justify-between md:items-center ">
                     {data?.bulletPoints?.map((item, index) => (
-                        <li key={index} className="text-gray-400 text-base">
-                            <span className=" text-[#455CFF]">{item.title}</span>: {item.description}
+                        <li key={index} className="flex flex-col md:flex-1 text-center text-gray-400 text-base">
+                            <span className=" text-[#455CFF]">{item.title}</span> {item.description}
                         </li>
                     ))}
                 </ul>
-                <p className="text-gray-300 text-lg">
+                <p className="text-gray-300 pt-2 text-sm italic">
                     Leave blank if a module does not apply.
                 </p>
             </div>
