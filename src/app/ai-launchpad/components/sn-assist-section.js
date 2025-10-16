706a4f51cff6701c8791959a70f3008cd648efce
ragritti <116258047+ragritti@users.noreply.github.com>
2025-10-16 10:33:42 +0530

    fixed ai assist diagram on ipad pro

--- src/app/ai-launchpad/components/sn-assist-section.js
+++ src/app/ai-launchpad/components/sn-assist-section.js
@@ -8,8 +8,8 @@ export default function SnAssistSection() {
     }
     return (
         <section className="relative md:py-16 py-10 ">
-            <div className="flex flex-col lg:flex-row items-center gap-4">
-                <div className="lg:w-1/3 flex flex-col gap-4">
+            <div className="flex flex-col xl:flex-row items-center gap-4">
+                <div className="xl:w-1/3 flex flex-col gap-4">
                     <h2 className="text-3xl md:text-5xl font-bold mb-4 lg:mb-0 text-white">
                         {data.title}
                     </h2>
@@ -17,7 +17,7 @@ export default function SnAssistSection() {
                         {data.description}
                     </p>
                 </div>
-                <div className="lg:w-2/3 ">
+                <div className="xl:w-2/3 ">
                     <ServiceNowAssistDiagram />
                 </div>
             </div>
